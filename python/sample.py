35995ecf52e0117d90771c090036baf6027f8eba
imartinez <inaki.martinez@zivautomation.com>
2020-09-25 09:44:30 +0200

    Add feature that indets when line finish with comma

--- python/sample.py
+++ python/sample.py
@@ -3,8 +3,6 @@ import vim
 def opened_parenthesis(line, cursor):
     found  = 0
     position = 0
-    print('B Cursor', line[:cursor])
-    print('A Cursor', line[cursor:])
     for i, letter in enumerate(reversed(line[:cursor])):
         if (letter == '('):
             found = found + 1
@@ -13,11 +11,27 @@ def opened_parenthesis(line, cursor):
             found = found - 1
 
     position = len(line[:cursor]) - position 
-    print (found, position, line[position])
     if (found > 0):
         return True,position
     return False,0
+
+def line_ended_in_comma(line, cursor):
+    print(line, ' ', cursor)
+    striped_line = line[:cursor].rstrip()
+    #Remove tabs to check if the line stars with a space
+    notab_line = line.replace('\t', '')
+    print (striped_line[len(striped_line)-1])
+    if ((striped_line[len(striped_line)-1] == ',') and notab_line[0] == ' '):
+        position = len(notab_line) - len(notab_line.lstrip())
+        return True,position
+    else:
+        return False,0
+
     
+#TODO mantain the TAB at the beginning of the previous line
+def edit_file(cb, line, colum, row, indexation_string):
+    cb[row] = line[:colum].rstrip()
+    cb.append(indexation_string+line[colum:].strip(),row+1)
 
 
 def print_country():
@@ -29,12 +43,16 @@ def print_country():
     colum = pos[1]+1
     line = cb[row]
     ret = opened_parenthesis(line, colum)
-    print (ret)
     spaces = ""
     if (ret[0] == True):
-        cb[row] = line[:colum]
-        cb.append(spaces.rjust(ret[1],'-')+line[colum:].strip(),row+1)
+        edit_file(cb, line, colum, row, spaces.rjust(ret[1],' '))
         vim.command('let changeIndex = 1')
     else:
-        vim.command('let changeIndex = 0')
+        ret = line_ended_in_comma(line, colum)
+        print (ret)
+        if (ret[0] == True):
+            edit_file(cb, line, colum, row, spaces.rjust(ret[1],' '))
+            vim.command('let changeIndex = 1')
+        else:
+            vim.command('let changeIndex = 0')
     #print ('You seem to be in %s' % _get_country())
